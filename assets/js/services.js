b006d658d538f100f06f8ae5b43c935cf4f58239
suhila <sofa6099@gmail.com>
2021-05-16 08:02:00 +0200

    simi final

--- assets/js/services.js
+++ assets/js/services.js
@@ -10,7 +10,6 @@ $( document ).ready(function() {
         e.preventDefault()
 
         var total = $("#plan").val()
-        alert (total)
         if (total == " باقه بلاتينية"){
             total = 1200
         }else{
@@ -21,45 +20,27 @@ $( document ).ready(function() {
         
         $("#form").prepend(' <input name="order_number" type="text" value=" '+ order_num + '" style="display:none" required="required">        ')
         Swal.fire({
-            title: 'قم باختيار طريقه الدفع',
+            title: 'اختر طريقه الدفع',
             showDenyButton: true,
-            confirmButtonText: `paypal`,
-            denyButtonText: `تحويل بنكي`,
+            confirmButtonText: `تحويل بنكي`,
+            denyButtonText: `الغاء `,
           }).then((result) => {
             
             if (result.isConfirmed) {
-                Swal.fire('قم بتحوبل '
-                + total + 
-                ' الي حسابنا في بايبال و سنبدأ العمل فورا',
-
-                'حساب باي بال : احتاجه'
-
-                ).then((result) => {
+                Swal.fire(
+                    ' قم بتحويل ' 
+                    +  total +
+                    ' الي حسابنا و سنبدأ  العمل فورا <br> ' ,
+                     " حساب البنكي : 0315012166660013 <br> (بنك مسقط)"
+                    ).then((result) => {
 
                     Swal.fire(
                         'بعد تحويلك تحدث معنا عبر واتساب وارسل ال recipt مع رقم الطلب : <br> '
                         +
                         order_num ,
-                        '<a href="https://wa.me/+96894252504">96894252504 <i class="fab fa-whatsapp"></i></a>',
+                        '<a href="https://wa.me/0096898988995">0096898988995 <i class="fab fa-whatsapp"></i></a>',
                         'success'
                     ).then((result) => { $(this).unbind('submit').submit(); })
-                });
-            } else if (result.isDenied) {
-                Swal.fire(
-                    ' قم بتحويل ' 
-                    +  total +
-                    ' الي حسابنا و سنبدأ  العمل فورا <br> ' ,
-                     " حساب البنكي : 0352029020460014 <br> (بنك مسقط)"
-                    ).then((result) => {
-
-                        Swal.fire(
-                            'بعد تحويلك تحدث معنا عبر واتساب وارسل ال recipt مع رقم الطلب : <br> '
-                            +
-                            order_num ,
-                            '<a href="https://wa.me/+96894252504"  target="_blank" >96894252504 <i class="fab fa-whatsapp"></i></a>',
-                            'success'
-
-                        ).then((result) => { $(this).unbind('submit').submit(); })
                 });
             }
             
